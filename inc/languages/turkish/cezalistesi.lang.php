0eceda71c5fd4dab16894cd38d6c9f01bf84218e
Kerem <kerem.er35@gmail.com>
2019-01-15 23:19:30 +0300

    Create cezalistesi.lang.php

---
+++ inc/languages/turkish/cezalistesi.lang.php
@@ -0,0 +1,45 @@
+<?php
+
+$l['cezalistesi'] = 'Ceza Listesi';
+$l['ulke'] = 'Ülke';
+$l['cezatarihi'] = 'Ceza Tarih';
+$l['oyuncuadi'] = 'Oyuncu Adı';
+$l['cezasebebi'] = 'Ceza Sebebi';
+$l['cezaveren'] = 'Ceza Veren';
+$l['sure'] = 'Süre';
+
+$l['cl_istatistik'] = 'Toplam {1} cezanın {2} - {3} arası görüntüleniyor.';
+
+$l['gizli'] = 'Gizli';
+$l['suredoldu'] = 'Ceza süresi doldu';
+$l['kaldi'] = ' kaldı';
+$l['yetkili_silinmis'] = 'Yetkili silinmiş';
+$l['sunucu_silinmis'] = 'Sunucu silinmiş';
+$l['isim_silinmis'] = 'İsim silinmiş';
+$l['bilinmiyor'] = 'Bilinmiyor';
+$l['ipadresi'] = 'IP Adresi';
+$l['baslangic'] = 'Başlangıç Tarihi';
+$l['bitis'] = 'Bitiş Tarihi';
+$l['sinirsiz'] = '<i class="fas fa-infinity"></i>';
+$l['sinirsizyazi'] = 'Sınırsız';
+$l['sure'] = 'Ceza Süresi';
+$l['cezayeri'] = 'Ceza Yeri';
+$l['kaydedildi'] = 'Değişiklikler başarıyla kaydedildi.';
+$l['kaydet'] = 'Değişiklikleri Kaydet';
+$l['duzenle'] = 'Düzenle';
+$l['sil'] = 'Sil';
+$l['kaldir'] = 'Kaldır';
+$l['cezakaldir'] = 'Cezayı kaldır';
+$l['oyuncu_adi'] = 'Oyuncu Adı';
+$l['steamid'] = 'SteamID';
+
+$l['cl_nav'] = "Navigasyon";
+
+$l['kayitbulunamadi'] = 'Böyle bir kayıt bulunamadı.';
+$l['dakika'] = ' dk';
+$l['cezasebebiyok'] = 'Ceza sebebini girmelisiniz.';
+
+$l['cezayikaldir'] = 'Cezayı kaldırmak istediğine eminmisin?';
+$l['cezayikaldironaybaslik'] = 'Ceza başarıyla kaldırıldı.';
+$l['cezayikaldironay'] = 'Başlangıç sayfasına yönlendiriliorsun.';
+$l['cezayikaldirret'] = 'Cezayı kaldırırken hata oluştu.';
